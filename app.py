ba92ce18ea67ada928743cf0e8d2de206869bd96
김민규 <wls8146com@gmail.com>
2023-10-11 19:35:08 +0900

    민규 rebase

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, session, redirect, flash, jsonify
+from pymongo import MongoClient
+import bcrypt
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return '정글에서 살아남기'
+   return render_template('index.html')
+
+@app.route('/select_team')
+def selectTeam():
+   return render_template('select_team.html')
+
+@app.route('/click_start_date.html')
+def createTeam():
+   return render_template('create_team.html')
+
+
+
+@app.route('/api/list', methods=['GET'])
+def getGroup():
+   groups = list(db.group.find({}, {'_id: False'}).sort('name', -1))
+   return jsonify({'result': 'success', 'group': groups})
+   return render_template('select_team.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/forgot_pwd')
+def forgot_pwd():
+    return render_template('forgot_pwd.html')
+
+@app.route('/sign_in')
+def sign_in():
+   return render_template('sign_in.html')
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
