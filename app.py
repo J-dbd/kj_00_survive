b2254702e029364170165375ed4235e74f1e3753
1stApplePie <chanwoo6422@naver.com>
2023-10-11 10:13:58 +0900

    Initial commit

--- app.py
+++ app.py
@@ -15,6 +15,12 @@ def home():
 def getGroup():
    groups = list(db.group.find({}, {'_id: False'}).sort('name', -1))
    return jsonify({'result': 'success', 'group': groups})
+   return render_template('select_team.html')
+
+@app.route('/api/list', methods=['GET'])
+def getGroup():
+   groups = list(db.group.find({}, {'_id: False'}).sort('name', -1))
+   return jsonify({'result': 'success', 'group': groups})
 
 
 @app.route('/')
@@ -23,16 +29,16 @@ def hello_world():  # put application's code here
 
 
 @app.route('/login')
-def login():  
+def login():
     return render_template('login.html')
 
 @app.route('/forgot_pwd')
-def forgot_pwd(): 
+def forgot_pwd():
     return render_template('forgot_pwd.html')
 
 @app.route('/sign_in')
 def sign_in():
    return render_template('sign_in.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
