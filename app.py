009fd8ed1d17bc660b1577bb76c01baffa0fb8f8
J_ManG <kjamong2020@gmail.com>
2023-10-11 10:49:33 +0900

    connect login findpassword

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ def get_week_number(date_str):
     try:
         # 날짜 문자열을 datetime 객체로 변환
         date = datetime.datetime.strptime(date_str, '%Y-%m-%d')
-        
+
         # 날짜로부터 해당 주차 계산
         week_number = date.isocalendar()[1]
-        
+
         return week_number
     except ValueError:
         return "날짜 형식이 잘못되었습니다."
@@ -68,15 +68,6 @@ def postTeam():
 
     date_str = '2023-10-10'
 
-    start_week_number = get_week_number(date_str)
-    present_week_number = get_week_number(translated_start_data)
-
-    week = present_week_number - start_week_number
-
-    # week 계산 함수 작성 필요
-    insert_dict = {"name": int(received_name), "start_date": translated_start_data, "end_date": translated_end_data, "team_member": {}, "week": week}
-    db.team.insert_one(insert_dict)
-    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+    app.run()
