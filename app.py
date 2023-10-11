adf2a16934dcb28a147af69bb325f134d087e4e6
J_ManG <kjamong2020@gmail.com>
2023-10-11 10:49:33 +0900

    connect login findpassword

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, session, redirect, flash, jsonify
+from pymongo import MongoClient
+import bcrypt
 
 app = Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+db = client.dbjungle
+
+@app.route('/')
+def home():
+   return render_template('select_team.html')
+
+@app.route('/api/list', methods=['GET'])
+def getGroup():
+   groups = list(db.group.find({}, {'_id: False'}).sort('name', -1))
+   return jsonify({'result': 'success', 'group': groups})
 
 
 @app.route('/')
