2a6283eb095cf100afe6bfa8754a4c677775d53d
J_ManG <kjamong2020@gmail.com>
2023-10-12 15:04:04 +0900

    login, signup, welcome

--- app.py
+++ app.py
@@ -53,13 +53,80 @@ def login():
    else:
       return render_template('login.html')
 
-@app.route('/forgot_pwd')
+#아이디/비밀번호 찾기
+@app.route('/forgot_pwd',methods=['GET','POST'])
 def forgot_pwd(): 
-    return render_template('forgot_pwd.html')
+   if request.method=='POST':
+      name=request.form['name']
+      email=request.form['email']
+      isUser=db['user'].find_one({'name':name,'email':email})
+      if isUser==None:
+         msg='존재하지 않는 유저입니다. 이름 혹은 이메일을 다시 확인해주세요.'
+         return jsonify({'result':'fail','reason':msg})
+      else:
+         user_id=isUser['id']
+         return jsonify({'result':'success','user_id':user_id})
+   else:
+      return render_template('forgot_pwd.html')
 
-@app.route('/sign_in')
+#아이디 찾기   
+@app.route('/find_id',methods=['GET','POST'])
+def find_id(): 
+   if request.method=='POST':
+      name=request.form['name']
+      email=request.form['email']
+      isUser=db['user'].find_one({'name':name,'email':email})
+      if isUser==None:
+         msg='이름 혹은 이메일을 다시 확인해주세요.'
+         return jsonify({'result':'fail','data':msg})
+      else:
+         user_id=isUser['id']
+         name=isUser['name']
+         data={'user_id':user_id,'name':name}
+         return jsonify({'result':'success','data':data})
+   else:
+      return jsonify({'result':'fail','data':'nothing'})
+
+#비밀번호 찾기 
+@app.route('/find_pwd',methods=['GET','POST'])
+def find_pwd(): 
+   if request.method=='POST':
+      id=request.form['id']
+      name=request.form['name']
+      email=request.form['email']
+      isUser=db['user'].find_one({'id':id,'name':name,'email':email})
+      if isUser==None:
+         msg='아이디, 이름, 이메일을 다시 확인해주세요.'
+         return jsonify({'result':'fail','data':msg})
+      else:
+         user_id=isUser['id']
+         name=isUser['name']
+         pwd=isUser['password']
+         data={'user_id':user_id,'name':name,'password':pwd}
+         return jsonify({'result':'success','data':data})
+   else:
+      return jsonify({'result':'fail','data':'nothing'})
+      
+#회원가입
+@app.route('/sign_in', methods=['GET','POST'])
 def sign_in():
-   return render_template('sign_in.html')
-
+   if request.method=='POST':
+      #print("request.form",request.form.to_dict())
+      id=request.form['id']
+      pw=request.form['password']
+      name=request.form['name']
+      email=request.form['email']
+      db['user'].insert_one({
+         'id':id,
+         'password':pw,
+         'name':name,
+         'email':email,
+      })
+      return jsonify({'result':'success'})
+   else:
+      return render_template('login.html')
+@app.route('/welcome')
+def welcome():
+   return render_template('welcome.html')
 if __name__ == '__main__':
     app.run(debug=True)
