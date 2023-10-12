8d9fb36c3c0a2205f2f9e24443da7e6ca4d75b3b
J_ManG <kjamong2020@gmail.com>
2023-10-13 04:26:05 +0900

    fix find pwd

--- app.py
+++ app.py
@@ -327,6 +327,43 @@ def delete_target():
 
     rtn_val = {'result': 'success'}
     return jsonify(rtn_val)
+ 
+@app.route('/api/postCheckboxStatus', methods=['POST'])
+def postCheckboxStatus():
+    received_arr = request.form['checkbox_status']
+    received_start_to_end_date = request.form['start_to_end_date']
+    received_team_number = request.form['team_number']
+
+    date_list = received_start_to_end_date.split('~')
+    start_date = date_list[0].rstrip()
+    end_date = date_list[-1].lstrip()
+
+    query = {
+       'start_date': start_date,
+       'end_date': end_date,
+       'team_number': int(received_team_number)
+    }
+
+    result_list = list(db.target_team.find(query))
+
+    if (len(result_list) != 0):
+        result = result_list[0]['_id']
+    else:
+       return jsonify({'result': 'fail'})
+    
+    sliced_arr = received_arr[1:-1]
+    splited_arr = received_arr.split(",")
+    for i in range(len(splited_arr)):
+        if i == 0:
+          splited_arr[i] = splited_arr[i][1:]
+        elif i == len(splited_arr)-1:
+          splited_arr[i] = splited_arr[i][:1]
+    int_arr = [int(i) for i in splited_arr]
+    db.target_team.update_one(
+      {"_id": ObjectId(result)}, 
+       {"$set": {"state": int_arr}}
+    )
+    return jsonify({'result': 'success'})
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
