1bc729e777a6a907394a60e4c534f881c2d02f1f
김민규 <wls8146com@gmail.com>
2023-10-12 20:18:37 +0900

    update

--- app.py
+++ app.py
@@ -5,31 +5,38 @@ import bcrypt
 import jwt
 from datetime import datetime, timedelta
 from functools import wraps
-import os 
+import os
 from dotenv import load_dotenv
 from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
-client = MongoClient('mongodb://minkyu:jungle@52.78.30.81',27017)
+load_dotenv()
+app.config['SECRET_KEY']=os.getenv('SECRET_KEY')
+MONGO_DB=os.getenv('MONGO_DB')
+client = MongoClient(MONGO_DB, 27017)
 
 db = client.jungle
 
-def get_week_number(date_str):
-    try:
-        # 날짜 문자열을 datetime 객체로 변환
-        date = datetime.datetime.strptime(date_str, '%Y-%m-%d')
+@app.route('/api/list', methods=['GET'])
+def getGroup():
+   groups = list(db.group.find({}, {'_id: False'}).sort('name', -1))
+   print(groups)
+   return jsonify({'result': 'success', 'group': groups})
 
-        # 날짜로부터 해당 주차 계산
-        week_number = date.isocalendar()[1]
+          # 날짜로부터 해당 주차 계산
+        #week_number = date.isocalendar()[1]
 
-        return week_number
-    except ValueError:
-        return "날짜 형식이 잘못되었습니다."
+
+        #return week_number
+    #except ValueError:
+        #return "날짜 형식이 잘못되었습니다."
+
+
 
 #####################
 #회원가입과 환영페이지 #
-######################      
+######################
 #회원가입
 @app.route('/sign_in', methods=['GET','POST'])
 def sign_in():
@@ -83,14 +90,14 @@ def login():
          elif isUser and isUser['password']!=pw:
             msg="비밀번호가 올바르지 않습니다."
          return jsonify({'result':'fail','reason':msg})
-      
+
    else:
       return render_template('login.html')
-   
+
 #JWT토큰 검증 API
 @app.route('/validator',methods=['POST'])
 def validator():
-   
+
    header_data=request.form['token']
    print('header_data',header_data)
    try:
@@ -101,7 +108,7 @@ def validator():
    except jwt.ExpiredSignatureError:
       msg='Signature has expired'
       return jsonify({'result':"fail",'data':msg})
-   
+
 
 ##############
 # ID/PW 찾기 #
@@ -109,7 +116,7 @@ def validator():
 
 #아이디/비밀번호 찾기
 @app.route('/forgot_pwd',methods=['GET','POST'])
-def forgot_pwd(): 
+def forgot_pwd():
    if request.method=='POST':
       name=request.form['name']
       email=request.form['email']
@@ -123,9 +130,9 @@ def forgot_pwd():
    else:
       return render_template('forgot_pwd.html')
 
-#아이디 찾기   
+#아이디 찾기
 @app.route('/find_id',methods=['GET','POST'])
-def find_id(): 
+def find_id():
    if request.method=='POST':
       name=request.form['name']
       email=request.form['email']
@@ -141,9 +148,9 @@ def find_id():
    else:
       return jsonify({'result':'fail','data':'nothing'})
 
-#비밀번호 찾기 
+#비밀번호 찾기
 @app.route('/find_pwd',methods=['GET','POST'])
-def find_pwd(): 
+def find_pwd():
    if request.method=='POST':
       id=request.form['id']
       name=request.form['name']
@@ -164,6 +171,9 @@ def find_pwd():
 @app.route('/select_team')
 def select_team():
    return render_template('select_team.html')
+
+
+
 
 @app.route('/')
 def home():
@@ -219,44 +229,20 @@ def postTeam():
 
     week = present_week_number - start_week_number
 
+    # week 계산 함수 작성 필요
     insert_dict = {"number": int(received_number), "start_date": translated_start_data, "end_date": translated_end_data, "team_member": {}, "week": week}
     db.team.insert_one(insert_dict)
     return jsonify({'result': 'success'})
 
-@app.route('/api/postNewTeamGoal', methods=['POST'])
-def postNewTeamGoal():
-    received_start_to_end_date = request.form['start_to_end_date']
-    received_team_number = request.form['team_number']
-    received_new_team_goal = request.form['text']
-
-    date_list = received_start_to_end_date.split('~')
-    start_date = date_list[0].rstrip()
-    end_date = date_list[-1].lstrip()
-
-    query = {
-       'start_date': start_date,
-       'end_date': end_date,
-       'team_number': int(received_team_number)
-    }
-
-    result_list = list(db.target_team.find(query))
-    if (len(result_list) != 0):
-        result = result_list[0]['_id']
-    else:
-       return jsonify({'result': 'fail'})
-
-    db.target_team.update_one(
-       {"_id": ObjectId(result)}, 
-       {"$push": {"text": received_new_team_goal}}
-    )
-    return jsonify({'result': 'success'})
-
-
-@app.route('/target_list')
+@app.route('/target_list', methods=['GET'])
 def target_list():
-    team = {"team_member": ['minkyu', 'chanu'], "start_date": "2023-10-10", "name": 3}
+    _id = request.args.get('_id')
+    member_name = db.user.find_one({"id":_id}, {"name":1, "_id":0})
+    member_name = member_name['name']
+    date = request.args.get('date')
+    team = {"team_member": ['minkyu', 'chanu'], "start_date": "2023-10-"+date, "number": 3}
 
-    data = list(db.target.find({}, {"_id":1, "member_id":1, "text":1, "state":1}))
+    data = list(db.target.find({"target_date": "2023-10-"+date}, {"_id":1, "member_id":1, "text":1, "state":1}))
     result = {}
     total_count = len(data)
     cnt = 0
@@ -274,22 +260,22 @@ def target_list():
             result[item['member_id']].append(text_state)
     print(result)
 
-    percentage = cnt/total_count * 100
-    datetime_str = "2023-10-11"
-    date_format = '%Y-%m-%d'
+    if cnt == 0:
+        percentage = 0
+    else:
+        percentage = cnt / total_count * 100
+
+    datetime_str = "2023-10-"+date+" 23:59:59"
+    date_format = '%Y-%m-%d %H:%M:%S'
 
     date_obj = datetime.strptime(datetime_str, date_format)
 
-    print(date_obj)
-    print(datetime.now())
-
-    return render_template('target_list.html',result=result, percentage = percentage, now = datetime.now(), current = date_obj, member_id="kyumin", target_date="2023-10-11")
+    return render_template('target_list.html',result=result, percentage = percentage, now = datetime.now(), current = date_obj, member_id=_id, target_date="2023-10-"+date, _id = member_name, date = date)
 
 @app.route('/api/check', methods=['POST'])
 def is_checked():
    obj_id = request.form['obj_id']
    state = request.form['state']
-   print(obj_id)
    # tgt = list(db.target.find({"_id": ObjectId(obj_id)}));
    if state == "checked":
        db.target.update_one({"_id": ObjectId(obj_id)}, {"$set": {"state": False}})
@@ -307,7 +293,6 @@ def create_new_target():
         'text': request.form['text'],
         'state': False
     }
-    print(_dict)
     db.target.insert_one(_dict)
 
     rtn_val = {'result': 'success'}
