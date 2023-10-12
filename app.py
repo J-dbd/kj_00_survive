70afec03bac4182fb2e1a6b82a78f80b6c5f2bca
김민규 <wls8146com@gmail.com>
2023-10-12 13:49:42 +0900

    팀 주간목표 페이지

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for, request, session, redirect, flash, jsonify
-from pymongo import MongoClient
+from pymongo import MongoClient, UpdateOne
+from bson.objectid import ObjectId
 import bcrypt
+from datetime import datetime, date
 
 app = Flask(__name__)
 
@@ -44,7 +46,7 @@ def selectTeam():
 def target_list():
     team = {"team_member": ['minkyu', 'chanu'], "start_date": "2023-10-10", "name": 3}
 
-    data = list(db.target.find({}, {"_id":0, "member_id":1, "text":1, "state":1}))
+    data = list(db.target.find({}, {"_id":1, "member_id":1, "text":1, "state":1}))
     result = {}
     total_count = len(data)
     cnt = 0
@@ -54,7 +56,7 @@ def target_list():
             cnt += 1
         else:
             item['state'] = ""
-        text_state = [item['text'], item['state']]
+        text_state = [item['text'], item['state'], item['_id']]
         if not result.get(item['member_id']):
             result[item['member_id']] = []
             result[item['member_id']].append(text_state)
@@ -63,8 +65,15 @@ def target_list():
     print(result)
 
     percentage = cnt/total_count * 100
+    datetime_str = "2023-10-11"
+    date_format = '%Y-%m-%d'
 
-    return render_template('target_list.html',result=result, percentage = percentage)
+    date_obj = datetime.strptime(datetime_str, date_format)
+
+    print(date_obj)
+    print(datetime.now())
+
+    return render_template('target_list.html',result=result, percentage = percentage, now = datetime.now(), current = date_obj, member_id="kyumin", target_date="2023-10-11")
 
 @app.route('/target_team_list')
 def target_team_list():
@@ -91,6 +100,45 @@ def target_team_list():
     percentage = cnt/total_count * 100
 
     return render_template('target_list.html',result=result, percentage = percentage)
+
+
+@app.route('/api/check', methods=['POST'])
+def is_checked():
+   obj_id = request.form['obj_id']
+   state = request.form['state']
+   print(obj_id)
+   # tgt = list(db.target.find({"_id": ObjectId(obj_id)}));
+   if state == "checked":
+       db.target.update_one({"_id": ObjectId(obj_id)}, {"$set": {"state": False}})
+   else:
+       db.target.update_one({"_id": ObjectId(obj_id)}, {"$set": {"state": True}})
+
+   rtn_val = {'result': 'success'}
+   return jsonify(rtn_val)
+
+@app.route('/api/add_new_target', methods=['POST'])
+def create_new_target():
+    _dict = {
+        'member_id': request.form['member_id'],
+        'target_date': request.form['target_date'],
+        'text': request.form['text'],
+        'state': False
+    }
+    print(_dict)
+    db.target.insert_one(_dict)
+
+    rtn_val = {'result': 'success'}
+    return jsonify(rtn_val)
+
+@app.route('/api/delete_target', methods=['POST'])
+def delete_target():
+    _dict = {
+        '_id': ObjectId(request.form['obj_id'])
+    }
+    db.target.delete_one(_dict)
+
+    rtn_val = {'result': 'success'}
+    return jsonify(rtn_val)
 
 if __name__ == '__main__':  
    app.run('0.0.0.0',port=5000,debug=True)
--- static/index.js
+++ static/index.js
@@ -1,3 +1,41 @@
-function is_checked(){
+function is_checked(obj_id, state){
+    $.ajax({
+        type:"POST",
+        url: "api/check",
+        data: {
+            'obj_id': obj_id,
+            'state': state,
+        },
+        success: function(response){
+            location.reload();
+        }
+    });
+}
 
+function add_new_target(member_id="KYUMIN", target_date="2023-10-10"){
+    $.ajax({
+        type:"POST",
+        url: "api/add_new_target",
+        data: {
+            'member_id': member_id,
+            'target_date' : target_date,
+            'text': document.getElementById("created_text").value
+        },
+        success: function(response){
+            location.reload();
+        }
+    });
+}
+
+function delete_item(obj_id){
+    $.ajax({
+        type:"POST",
+        url: "api/delete_target",
+        data: {
+            'obj_id': obj_id
+        },
+        success: function(response){
+            location.reload();
+        }
+    });
 }
